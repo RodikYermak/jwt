fda5d4df8074437ea8389f42ea8271f7c73d1da9
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2024-05-21 21:06:40 -0700

    folders structure controllers models router service

--- server/index.js
+++ server/index.js
@@ -3,11 +3,22 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
+const mongoose = require('mongoose');
+
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
 const PORT = process.env.PORT || 5100;
 
 const start = async () => {
     try {
+        await mongoose.connect(process.env.DB_URL, {
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
+        });
+
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
     } catch (e) {
         console.log(e);
