28d0acbd84b3984669771a0dcf1293a56d84358a
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2024-05-23 01:10:26 -0700

    generating access and refresh tokens

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(cors());
 app.use('/api', router);
 
-const PORT = process.env.PORT || 5100;
+const PORT = process.env.PORT || 5101;
 
 const start = async () => {
     try {
@@ -28,4 +28,3 @@ const start = async () => {
 };
 
 start();
-// ds
