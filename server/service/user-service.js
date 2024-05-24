250a0563d6d5527f170731b2673fabb30b014710
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2024-05-23 19:59:41 -0700

    logout

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -43,6 +43,31 @@ class UserService {
         user.isActivated = true;
         await user.save();
     }
+
+    async login(email, password) {
+        const user = await UserModel.findOne({ email });
+        if (!user) {
+            throw ApiError.BadRequest('User not found');
+        }
+        const isPassEquals = await bcrypt.compare(password, user.password);
+
+        if (!isPassEquals) {
+            throw ApiError.BadRequest('Incorrect password');
+        }
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({ ...userDto });
+
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+        return {
+            ...tokens,
+            user: userDto,
+        };
+    }
+
+    async logout(refreshToken) {
+        const token = await tokenService.removeToken(refreshToken);
+        return token;
+    }
 }
 
 module.exports = new UserService();
